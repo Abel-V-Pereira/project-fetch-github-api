0b95d591f811441cb51966ca6713c74bb2b0d35d
Abel Viegas Pereira <abelviegaspereira@gmail.com>
2024-09-15 17:15:56 -0300

    primeiro commit

---
+++ src/scripts/objects/screen.js
@@ -0,0 +1,30 @@
+const screen = {
+    userProfile: document.querySelector('.profile-data'),
+    renderUser(user) {
+        this.userProfile.innerHTML = `
+        <div class="info">
+            <img src="${user.avatarUrl}" alt="Foto do perfil de usuário" />
+            <div class="data">
+                <h1>${user.name ?? 'Não possui nome cadastrado 😭'}</h1>
+                <p>${user.bio ?? 'Não possui bio cadastrada 😭'}</p>
+            </div>
+        <div>`
+
+        let repositoriesItens = ''
+        user.repositories.forEach(repo => repositoriesItens += `
+            <li><a href="${repo.html_url}" target="_blank">${repo.name}</a></li>`)
+
+        if (user.repositories.length > 0) {
+            this.userProfile.innerHTML += `
+            <div class="repositories section">
+                <h2>Repositórios</h2>
+                <ul>${repositoriesItens}</ul>
+            </div>`
+        }
+    },
+    renderNotFound(){
+        this.userProfile.innerHTML = '<h3>Usuário não encontrado</h3>'
+    }
+}
+
+export { screen }
